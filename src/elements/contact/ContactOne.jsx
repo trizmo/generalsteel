7df7d6f22985b956a7d090836d6143f7d4fcda9d
Tristan Perera <tristan.e.perera@gmail.com>
2022-03-03 18:25:57 -0800

    updated env var

--- src/elements/contact/ContactOne.jsx
+++ src/elements/contact/ContactOne.jsx
@@ -72,7 +72,7 @@ class ContactOne extends Component{
                         </div>
                         <div className="col-lg-6 order-1 order-lg-2">
                             <div className="thumbnail mb_md--30 mb_sm--30">
-                                <img src="/assets/images/about/about-6.jpg" alt="trydo"/>
+                                <img src="/assets/images/about/about-6.jpg" alt="General Steel"/>
                             </div>
                         </div>
                     </div>
